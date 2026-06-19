245fc82712e731527c8d9b686699c3a5c86335f3
Gabriel Almeida <gabrielalmeidaalexandreitu@gmail.com>
2026-06-19 20:35:06 -0300

    feat: migra model Cliente do Django para JPA

--- src/main/java/com/adkdevelopment_test/application/model/Cliente.java
+++ src/main/java/com/adkdevelopment_test/application/model/Cliente.java
@@ -1,6 +1,7 @@
 package com.adkdevelopment_test.application.model;
 
 import jakarta.persistence.*;
+import org.hibernate.annotations.CreationTimestamp;
 
 import java.time.LocalDateTime;
 
@@ -11,6 +12,66 @@ public class Cliente {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    public Cliente(){
+
+    };
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public Estado getEstado() {
+        return estado;
+    }
+
+    public void setEstado(Estado estado) {
+        this.estado = estado;
+    }
+
+    public LocalDateTime getCriadoEm() {
+        return criadoEm;
+    }
+
+    public void setCriadoEm(LocalDateTime criadoEm) {
+        this.criadoEm = criadoEm;
+    }
+
     private String name;
     private String email;
     private String telefone;
@@ -19,5 +80,7 @@ public class Cliente {
     @Enumerated(EnumType.STRING)
     private Estado estado;
 
+
+    @CreationTimestamp
     private LocalDateTime criadoEm;
 }
